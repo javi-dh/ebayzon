035a1b41fe88888ac6948633aeb0dec5c85ea16d
dcaplandh <dcaplan@digitalhouse.com>
2018-12-07 15:08:08 -0300

    Routes resource example

--- routes/web.php
+++ routes/web.php
@@ -12,17 +12,22 @@ use App\Brand;
 |
 */
 
-Route::get('/products', function () {
-	$products = \App\Product::all();
-   return view('products')->with(compact('products'));
-});
+// Route::get('/brands', function () {
+// 	$brands = \App\Brand::all();
+//    return view('brands')->with(compact('brands'));
+// });
+//
+// Route::get('/colors', function () {
+// 	$colors = \App\Color::all();
+//    return view('colors')->with(compact('colors'));
+// });
 
-Route::get('/brands', function () {
-	$brands = \App\Brand::all();
-   return view('brands')->with(compact('brands'));
-});
-
-Route::get('/colors', function () {
-	$colors = \App\Color::all();
-   return view('colors')->with(compact('colors'));
-});
+Route::get('/products/api', 'ProductsController@api');
+Route::resource('/products', 'ProductsController');
+// Route::get('/products', 'ProductsController@index');
+// Route::post('/products', 'ProductsController@store');
+// Route::get('/products/create', 'ProductsController@create');
+// Route::get('/products/{id}', 'ProductsController@show');
+// Route::put('/products/{id}', 'ProductsController@update');
+// Route::delete('/products/{id}', 'ProductsController@destroy');
+// Route::get('/products/{id}/edit', 'ProductsController@edit');
